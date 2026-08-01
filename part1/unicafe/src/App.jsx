46f8afd368e62596c3887d0be020096850db5e43
Lena Perzlmaier <lenalein@posteo.de>
2026-08-01 11:48:37 +0200

    complete exercise 1.10 and 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -15,6 +15,7 @@ const Statistics = (props) => {
       </div>
     )
   }
+
   const percentagePositive = () => {
     return props.good / total * 100
   }
@@ -22,19 +23,27 @@ const Statistics = (props) => {
   const averageScore = () => {
     return (props.good - props.bad) / total
   }
+
   return (
-    <div>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>total {total}</p>
-      <p>Feedback score {averageScore()} </p>
-      <p>Percentage of positive feedback {percentagePositive()}%</p>
-    </div>
+    <table>
+      <tbody>
+        <StatisticsLine text="good" value={props.good} />
+        <StatisticsLine text="neutral" value={props.neutral} />
+        <StatisticsLine text="bad" value={props.bad} />
+        <StatisticsLine text="total" value={total} />
+        <StatisticsLine text="Feedback score" value={averageScore()} />
+        <StatisticsLine text="Percentage of positive feedback" value={percentagePositive() + "%"} />
+      </tbody>
+    </table>
   )
 }
 
-
+const StatisticsLine = (props) => {
+  return (<tr>
+    <td>{props.text}</td>
+    <td>{props.value}</td>
+  </tr>)
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -45,7 +54,6 @@ const App = () => {
   const handleGood = () => setGood(good + 1)
   const handleNeutral = () => setNeutral(neutral + 1)
   const handleBad = () => setBad(bad + 1)
-
 
   return (
     <div>
