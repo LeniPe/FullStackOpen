1159dc819e8cfd63784f3459664c6f4a500c4bb0
Lena Perzlmaier <lenalein@posteo.de>
2026-08-05 19:51:03 +0200

    complete exercises 2.7 to 2.10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,43 +1,95 @@
 import { useState } from 'react'
 
+const Entries = ({ entries, searchTerm }) => {
+  return (
+    <div>
+      {entries.map(entry =>
+        <Entry key={entry.name} entry={entry} searchTerm={searchTerm} />
+      )}
+    </div>
+  )
+}
+
+const Entry = ({ entry, searchTerm }) => {
+  if (entry.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+    return (<p> {entry.name} {entry.number}</p>)
+  }
+  else return null
+}
+
+const AddEntryForm = ({ newName, newNumber, handleNameChange, handleNumberChange, onSubmit }) => {
+  return (
+    <>
+      <h2>Add Entry</h2>
+      <form onSubmit={onSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    </>
+  )
+}
+
+const SearchForm = ({ searchTerm, onChange }) => {
+  return (
+    <div>
+      search names: <input value={searchTerm} onChange={onChange} />
+    </div>)
+}
+
 const App = () => {
-  
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+
+  const [entries, setEntries] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
-  
+
   const [newName, setNewName] = useState('')
-  
+  const [newNumber, setNewNumber] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
+
   const handleNameChange = (event) => {
-    console.log(event.target.value)
     setNewName(event.target.value)
   }
-  
-  const addPerson = (event) => {
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearchTerm = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const addEntry = (event) => {
     event.preventDefault()
-    const nameObject = { name: newName }
-    setPersons(persons.concat(nameObject))
+    const names = entries.map(entry => entry.name)
+
+    if (names.includes(newName)) {
+      console.log("Name is already in phonebook!")
+      alert(`Name ${newName} is already in phonebook`)
+    }
+    else {
+      const newEntry = { name: newName, number: newNumber }
+      setEntries(entries.concat(newEntry))
+    }
     setNewName('')
+    setNewNumber('')
   }
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-        <div>debug: {newName} </div>
-      </form>
+      <h1>Phonebook</h1>
+      <AddEntryForm newName={newName} newNumber={newNumber} handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange} onSubmit={addEntry} />
       <h2>Numbers</h2>
-      <div>
-        {persons.map(person =>
-          <p key={person.name}> {person.name}</p>
-        )}
-      </div>
+      <SearchForm searchTerm={searchTerm} onChange={handleSearchTerm} />
+      <Entries entries={entries} searchTerm={searchTerm} />
     </div>
   )
 }
