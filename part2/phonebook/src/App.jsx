eb7dfd4d6f4b6cf3c5ba6161939b39c6ad35af4d
Lena Perzlmaier <lenalein@posteo.de>
2026-08-07 14:25:14 +0200

    complete exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Entries = ({ entries, searchTerm }) => {
   return (
@@ -43,13 +44,7 @@ const SearchForm = ({ searchTerm, onChange }) => {
 
 const App = () => {
 
-  const [entries, setEntries] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-
+  const [entries, setEntries] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
@@ -81,6 +76,15 @@ const App = () => {
     setNewName('')
     setNewNumber('')
   }
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setEntries(response.data)
+      })
+  }
+    , [])
 
   return (
     <div>
